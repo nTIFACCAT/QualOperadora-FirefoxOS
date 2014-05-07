2b3035aca692de881c31f1fc8636f14f1fc7ad57
Fernando Porazzi <fporazzi46@gmail.com>
2014-05-07 10:52:27 -0300

    Add mock
    Since I'm not getting a result from server due to a Cross Domain
    restriction, I build a mock to simulate the original server response.

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,10 +13,30 @@
       var phoneNumber = d.querySelector('#main__form-phone').value,
           xhr = new XMLHttpRequest();
 
-      xhr.open('GET', 'http://private-61fc-rodrigoknascimento.apiary-mock.com/consulta/5199999999', true);
+      // Since I am passing through some cross-domains problems, I've build a Mock to simulate te original behavior
+      //xhr.open('GET', 'http://private-61fc-rodrigoknascimento.apiary-mock.com/consulta/5199999999', true);
+
+      xhr.open('GET', './', true);
 
       xhr.onload = function () {
-        console.log(xhr.responseText);
+        //console.log(xhr.responseText);
+
+        // mock =/
+        var data = JSON.parse('{"telefone": "(51) 9999-9999","operadora": "Vivo - Celular","estado": "Rio Grande do Sul","portabilidade": false}');
+
+        if (data) {
+          var tree = '';
+
+          tree += '<p><span>Número:</span> ' + data.telefone + '</p>';
+          tree += '<p><span>Operadora:</span> ' + data.operadora + '</p>';
+          tree += '<p><span>Portabilidade:</span> ' + data.portabilidade === true ? 'Sim' : 'Não' + '</p>';
+          tree += '<p><span>Estado:</span> ' + data.estado + '</p>';
+
+          d.querySelector('#resp').innerHTML = tree;
+        } else {
+          d.querySelector('#resp').innerHTML = 'Não encontramos nenhum registro';
+        }
+
       };
 
       xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
